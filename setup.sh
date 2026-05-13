0de04089400b620939fe6a8b89f854b72e3bdafb
Hugo Marotta <humarotta@proton.me>
2026-05-12 23:37:08 -0300

    Fix Proton Pass download URL

--- setup.sh
+++ setup.sh
@@ -185,7 +185,7 @@ abbr > $__fish_config_dir/conf.d/abbreviations.fish
 '
 
 # Install Proton Pass
-sudo dnf install -y https://proton.me/download/pass/linux/proton-pass-1.36.1-1.x86_64.rpm
+sudo dnf install -y https://proton.me/download/PassDesktop/linux/x64/ProtonPass.rpm
 
 # Install Proton Authenticator
 sudo dnf install -y https://proton.me/download/authenticator/linux/ProtonAuthenticator-1.1.5-1.x86_64.rpm
